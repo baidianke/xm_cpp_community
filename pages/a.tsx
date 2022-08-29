c9a839c69358459756ad71311870f80a5fa5ec37
daief <daief@xiaoma.cn>
2022-08-29 15:30:00 +0800

    11

--- pages/a.tsx
+++ pages/a.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '@/styles/Home.module.less';
+import styles from '@/styles/Home.module.less'
 
 const PageA: NextPage = () => {
+  console.log("111")
   return (
     <div className={styles.container}>
       <Head>
